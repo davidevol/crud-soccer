c566190efaa638bf0f1d578c05054a5f3886d633
De <deehzi@pm.me>
2023-01-05 18:30:15 -0300

    responding to url parameters

--- app.js
+++ app.js
@@ -19,6 +19,27 @@ app.get('/api/v1/stadiums', (req, res) => {
   });
 });
 
+app.get('/api/v1/stadiums/:id', (req, res) => {
+    console.log(req.params)
+    const id = parseInt(req.params.id);
+    const stadium = stadiums.find(el => el.id === id);
+
+    if (!stadium){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(200).json({
+      status: 'sucess',
+      length: stadiums.length,
+      data: {
+        stadium
+      },
+    });
+  });
+
 app.post('/api/v1/stadiums', (req, res) => {
   const newId = stadiums[stadiums.length - 1].id + 1; // fazendo de forma manual pela inexistencia de banco de dados!
   const newStad = Object.assign({ id: newId }, req.body);
