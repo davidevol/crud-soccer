3db067032a9c8950a2a2057985c0bd939b06448e
De <deehzi@pm.me>
2023-01-08 14:52:45 -0300

    setting up a request rate limit

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit')
 
 const stadiumRouter = require('./routes/stadiumRoutes');
 
 const app = express();
 
+const requestLimiter = rateLimit({
+    max: 10,
+    message: "there are too many requests, please wait!"
+})
+
+
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use('/api/v1/stadiums', requestLimiter);
 app.use('/api/v1/stadiums', stadiumRouter);
 
 module.exports = app;
