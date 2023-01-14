6ab8d64358e826e3a372c7033eb83328046c0537
De <deehzi@pm.me>
2023-01-14 15:42:33 -0300

    feat: adding a handler for catching errors

--- controllers/stadiumController.js
+++ controllers/stadiumController.js
@@ -1,198 +1,143 @@
 const Stadium = require("./../models/stadiumModel");
 const APIFeatures = require("./../utils/apiFeatures");
+const catchAsync = require("./../utils/catchAsync");
 
-exports.aliasTopCheap = (req, res, next) => {
-  try {
-    req.query.limit = "5";
-    req.query.sort = "-ratingsAverage,price";
-    req.query.fields = "name,price,ratingsAverage,summary,difficulty";
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
+exports.aliasTopCheap = catchAsync(async (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
   next();
-};
+});
 
-exports.getAllStadiums = async (req, res) => {
-  try {
-    const features = new APIFeatures(Stadium.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const stadiums = await features.query;
+exports.getAllStadiums = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Stadium.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const stadiums = await features.query;
 
-    res.status(200).json({
-      status: "sucess",
-      length: stadiums.length,
-      data: {
-        stadiums,
+  res.status(200).json({
+    status: "sucess",
+    length: stadiums.length,
+    data: {
+      stadiums,
+    },
+  });
+});
+
+exports.getStadium = catchAsync(async (req, res, next) => {
+  const stadium = await Stadium.findById(req.params.id);
+  res.status(200).json({
+    status: "sucess",
+    data: {
+      stadium,
+    },
+  });
+});
+
+exports.addStadium = catchAsync(async (req, res, next) => {
+  const newStadium = await Stadium.create(req.body);
+
+  res.status(201).json({
+    status: "sucess",
+    data: {
+      stadium: newStadium,
+    },
+  });
+});
+
+exports.deleteStadium = catchAsync(async (req, res, next) => {
+  await Stadium.findByIdAndDelete(req.params.id);
+
+  res.status(204).end();
+});
+
+exports.changeStadium = catchAsync(async (req, res, next) => {
+  const stadium = await Stadium.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "sucess",
+    data: {
+      stadium,
+    },
+  });
+});
+
+exports.getStadiumStats = catchAsync(async (req, res, next) => {
+  const stats = await Stadium.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: { $toUpper: "$difficulty" },
+        numTours: { $sum: 1 },
+        numRatings: { $sum: "$ratingsQuantity" },
+        avgRating: { $avg: "$ratingsAverage" },
+        avgPrice: { $avg: "$price" },
+        minPrice: { $min: "$price" },
+        maxPrice: { $max: "$price" },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+    },
+    {
+      $sort: { avgPrice: 1 },
+    },
+  ]);
 
-exports.getStadium = async (req, res) => {
-  try {
-    const stadium = await Stadium.findById(req.params.id);
-    res.status(200).json({
-      status: "sucess",
-      data: {
-        stadium,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      stats,
+    },
+  });
+});
 
-exports.addStadium = async (req, res) => {
-  try {
-    const newStadium = await Stadium.create(req.body);
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1; // 2021
 
-    res.status(201).json({
-      status: "sucess",
-      data: {
-        stadium: newStadium,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-
-exports.deleteStadium = async (req, res) => {
-  try {
-    const stadium = await Stadium.findByIdAndDelete(req.params.id);
-
-    res.status(204).end();
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-
-exports.changeStadium = async (req, res) => {
-  try {
-    const stadium = await Stadium.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    res.status(200).json({
-      status: "sucess",
-      data: {
-        stadium,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-
-exports.getStadiumStats = async (req, res) => {
-  try {
-    const stats = await Stadium.aggregate([
-      {
-        $match: { ratingsAverage: { $gte: 4.5 } },
-      },
-      {
-        $group: {
-          _id: { $toUpper: "$difficulty" },
-          numTours: { $sum: 1 },
-          numRatings: { $sum: "$ratingsQuantity" },
-          avgRating: { $avg: "$ratingsAverage" },
-          avgPrice: { $avg: "$price" },
-          minPrice: { $min: "$price" },
-          maxPrice: { $max: "$price" },
+  const plan = await Stadium.aggregate([
+    {
+      $unwind: "$startDates",
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
         },
       },
-      {
-        $sort: { avgPrice: 1 },
+    },
+    {
+      $group: {
+        _id: { $month: "$startDates" },
+        numTourStarts: { $sum: 1 },
+        stadium: { $push: "$name" },
       },
-    ]);
+    },
+    {
+      $addFields: { month: "$_id" },
+    },
+    {
+      $project: {
+        _id: 0,
+      },
+    },
+    {
+      $sort: { numTourStarts: -1 },
+    },
+    {
+      $limit: 12,
+    },
+  ]);
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        stats,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const year = req.params.year * 1; // 2021
-
-    const plan = await Stadium.aggregate([
-      {
-        $unwind: "$startDates",
-      },
-      {
-        $match: {
-          startDates: {
-            $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-31`),
-          },
-        },
-      },
-      {
-        $group: {
-          _id: { $month: "$startDates" },
-          numTourStarts: { $sum: 1 },
-          stadium: { $push: "$name" },
-        },
-      },
-      {
-        $addFields: { month: "$_id" },
-      },
-      {
-        $project: {
-          _id: 0,
-        },
-      },
-      {
-        $sort: { numTourStarts: -1 },
-      },
-      {
-        $limit: 12,
-      },
-    ]);
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        plan,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      plan,
+    },
+  });
+});
