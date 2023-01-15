fc08a4d3ff17f70fb4cd9e7fb66725cbb3d7432c
De <deehzi@pm.me>
2023-01-15 18:47:23 -0300

    feat: adding a handler for invalids requests for production mode

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDevelopment = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -15,9 +35,12 @@ const sendErrorProduction = (err, res) => {
       message: err.message,
     });
 
-    // Programming error will don't leak error details
+    // Programming or other unknown error: don't leak error details
   } else {
+    // 1) Log error
     console.error("ERROR 💥", err);
+
+    // 2) Send generic message
     res.status(500).json({
       status: "error",
       message: "Something went very wrong!",
@@ -32,6 +55,13 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDevelopment(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProduction(err, res);
+    let error = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+
+    sendErrorProduction(error, res);
   }
 };
