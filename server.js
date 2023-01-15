f3a65f9b3c5579d190cc5b1a5f00f8064a45946f
De <deehzi@pm.me>
2023-01-15 18:53:00 -0300

    feat: adding a handler for unhandled rejections

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ mongoose
   .then(() => console.log("DB connected sucessful!"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
