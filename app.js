2b838bd9647d6b98553fc4a34947182a63c783c6
De <deehzi@pm.me>
2023-01-17 20:09:10 -0300

    feat: improving security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet")
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,6 +9,7 @@ const stadiumRouter = require("./routes/stadiumRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
+app.use(helmet())
 
 
 if (process.env.NODE_ENV === "development") {
@@ -21,7 +23,8 @@ const requestLimiter = rateLimit({
 });
 app.use("/api", requestLimiter);
 
-app.use(express.json());
+
+app.use(express.json( { limit: "10kb"} ));
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/stadiums", stadiumRouter);
