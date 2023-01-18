055eacf5e5301b87ddfe2e949f96fb4a01d9a5e2
De <deehzi@pm.me>
2023-01-18 15:29:08 -0300

    refactor: improving function name

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,15 @@ const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
-const filterObj = (obj, ...allowedFields) => {
-  const newObj = {};
-  Object.keys(obj).forEach((el) => {
-    if (allowedFields.includes(el)) newObj[el] = obj[el];
+const filterObject = (object, ...allowedFields) => {
+  const newObject = {};
+  Object.keys(object).forEach((el) => {
+    if (allowedFields.includes(el)) newObject[el] = object[el];
   });
-  return newObj;
+  return newObject;
 };
+
+Object
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -35,7 +37,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
-  const filteredBody = filterObj(req.body, "name", "email");
+  const filteredBody = filterObject(req.body, "name", "email");
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
