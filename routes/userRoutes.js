88b637e5e9c9762bdffebc0441cdea78c615313b
De <deehzi@pm.me>
2023-01-19 19:27:55 -0300

    feat: adding a /me endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,8 +16,15 @@ router.patch(
   authController.updatePassword
 );
 
-router.patch("/updateMe", authController.protect, userController.updateMe)
-router.delete("/deleteMe", authController.protect, userController.deleteMe)
+router.get(
+  "/me",
+  authController.protect,
+  userController.getMe,
+  userController.getUser
+);
+
+router.patch("/updateMe", authController.protect, userController.updateMe);
+router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
 router
   .route("/")
