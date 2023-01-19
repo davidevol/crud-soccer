080c4c101cca5430ff847ae7868485ace5dade5e
De <deehzi@pm.me>
2023-01-19 13:03:15 -0300

    feat: modelling reviews with parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Stadium = require("./stadiumModel");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Review can not be empty!"],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    stadium: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Stadium",
+      required: [true, "Review must belong to a stadium."],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
