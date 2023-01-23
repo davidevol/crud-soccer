3980f0c13f4f1450471b11c20b80969f03145adb
De <deehzi@pm.me>
2023-01-23 02:22:32 -0300

    feat: adding cookie parser lib

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -39,6 +40,7 @@ app.use("/api", requestLimiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser())
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -60,12 +62,7 @@ app.use(
   })
 );
 
-
-
 // 3) ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render("base")
-})
 
 app.use("/", viewRouter);
 app.use("/api/v1/stadiums", stadiumRouter);
