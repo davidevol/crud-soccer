3fc9262738227720591ad0f1468abb77721c4970
De <deehzi@pm.me>
2023-01-23 19:22:16 -0300

    feat: adding mapbox

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,9 +15,6 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    //if expires dont work use expiresIn
-    //expiresIn: process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
